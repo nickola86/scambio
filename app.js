d4808772489263292b534f4de186f5920f4d08da
Nicola <nicola.ditrani@gmail.com>
2017-12-18 15:08:11 +0100

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+ var express = require("express");
+ var app = express();
+
+ /* serves main page */
+ app.get("/app", function(req, res) {
+	console.log('GET  ' + req.url);
+	console.log('>>>> payload : ' + JSON.stringify(req.params));
+    res.sendFile( __dirname + '/app/index.html');
+ });
+
+  app.get("/servizi/test.json", function(req, res) { 
+	console.log('GET   ' + req.url);
+    console.log('>>>> payload : ' + JSON.stringify(req.params));
+	var objOK = {status:"OK", message:"Richiesta Elaborata Correttamente!!"};
+	var objKO = {status:"OK", message:"Richiesta terminata in errore!!", detail: "java.lang.NullPointerException : Line 56"};
+	if(Math.ceil(2*Math.random())==1)
+		res.send(objOK);
+	else
+		res.send(objKO);
+  });
+
+  app.post("/servizi/test.json", function(req, res) { 
+	console.log('POST  ' + req.url);
+    console.log('>>>> payload : ' + JSON.stringify(req.params));
+	var objOK = {status:"OK", message:"Richiesta Elaborata Correttamente!!"};
+	var objKO = {status:"OK", message:"Richiesta terminata in errore!!", detail: "java.lang.NullPointerException : Line 56"};
+	if(Math.ceil(2*Math.random())==1)
+		res.send(objOK);
+	else
+		res.send(objKO);
+  });
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+	 console.log('GET  ' + req.url);
+     console.log('>>>> payload : ' + JSON.stringify(req.params));
+     res.sendFile( __dirname + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
